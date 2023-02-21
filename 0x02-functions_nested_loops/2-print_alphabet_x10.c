a4d19b0b76f1f890e389e1e23c52b0a0f2d4d3b9
abelsnow13 <abelsnow13@gmail.com>
2023-02-21 12:21:45 -0800

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		char la = 'a';
+
+		while (la <= 'z')
+		{
+			_putchar(la);
+			la++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
