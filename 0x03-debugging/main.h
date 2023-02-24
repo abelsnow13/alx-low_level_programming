ea7b6257fd3ddfc324ac5ac733ef424ca1275eb4
abelsnow13 <abelsnow13@gmail.com>
2023-02-23 16:14:47 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
 
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int c);
+void jack_bauer(void);
+void times_table(void);
+void print_times_table(int n);
+int add(int a, int b);
+void print_to_98(int n);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
