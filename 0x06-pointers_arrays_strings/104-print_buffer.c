b14ec31d3740e061f7ddd1858a876ab222d4b44b
abelsnow13 <abelsnow13@gmail.com>
2023-03-02 13:12:55 -0800

    Commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,57 +1,46 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-*print_buffer -  C function that prints the content of an
-*  inputted number of bytes from a buffer.
-* Prints 10 bytes per line.
-* Starts with the position of the first byte in hexadecimal (8 chars),
-* starting with `0`.
-* Each line shows the hexadecimal content (2 chars) of the buffer,
-* 2 bytes at a time, separated by a space.
-* Each line shows the content of the buffer.
-* Prints the byte if it is printable; if not, prints `.`.
-* Each line ends with a new line `\n`.
-* If the inputted byte size is 0 or less, the function only prints a new line.
-*@b: number of bytes
-*@size: size of the byte
-*/
+ * print_buffer - print a buffer 10 byte at a tine
+ * @b: the buffer to be printd
+ * @size: the number of byte
+ * Return: nothing
+ */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+int byte, index;
 
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x; ", byte);
+for (byte = 0; byte < size; byte += 10)
+{
+printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf("  ");
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+printf("  ");
 
-			else
-				printf("%02x", *(b + index + byte));
+else
+printf("%02x", *(b + index + byte));
+if ((index % 2) != 0 && index != 0)
+printf(" ");
+}
 
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+break;
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size
-					break;
+else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+printf("%c", *(b + index + byte));
 
-					else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-					printf("%c", *(b + index + byte));
-					else
-					printf(".");
-					}
-
-					if (byte >= size)
-					continue;
-
-					print("\n");
-					}
-
-					if (size <= 0)
-					printf("\n");
-					}
-
+else
+printf(".");
+}
+if (byte >= size)
+continue;
+printf("\n");
+}
+if (size <= 0)
+printf("\n");
+}
