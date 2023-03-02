2406d9301cb4b3941afe879ad0cbe454bff91dd5
abelsnow13 <abelsnow13@gmail.com>
2023-03-02 13:09:50 -0800

    Commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,70 @@
 #include "main.h"
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
-char *cap_string(char *str)
+ * add_strings - add the number stored on two strings
+ * @n1: The string containing the first number
+ * @r: string
+ * @n2: - Second string
+ * @r_index: the current index of the buffer
+ *
+ * Return: Always success
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int index = 0;
+int num, tens = 0;
+for (; *n1 && *n2; n1--, n2--, r_index--)
+{
+num = (*n1 - '0') + (*n2 - '0');
+num += tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+for (; *n1; n1--, r_index--)
+{
+num = (*n1 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
 
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
+for (; *n2; n2--, r_index--)
+{
+num = (*n2 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+if (tens && r_index >= 0)
+{
+*(r + r_index) = (tens & 10) + '0';
+return (r + r_index);
+}
+else if (tens && r_index < 0)
+return (0);
+return (r + r_index + 1);
+}
 
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-			str[index] -= 32;
+/**
+ * infinite_add - add the number stored on two strings
+ * @n1: The string containing the first number
+ * @n2: - Second string
+ * @r: string
+ * @size_r: the current index of the buffer
+ *
+ * Return: Always success
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int index, n1_len = 0, n2_len = 0;
 
-		index++;
-	}
-	return (str);
+for (index = 0; *(n1 + index); index++)
+n1_len++;
+for (index = 0; *(n2 + index); index++)
+n2_len++;
+if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+return (0);
+
+n1 += n1_len - 1;
+n2 += n2_len - 1;
+*(r + size_r) = '\0';
+return (add_strings(n1, n2, r, --size_r));
 }
